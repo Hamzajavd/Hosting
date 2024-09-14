f20d05e2ec1342f8735e54942bfec241cb035844
Hamza Javd <javedhamza895@gmail.com>
2024-09-15 01:31:24 +0500

    Assignment

---
+++ public class Main {.java
@@ -0,0 +1,8 @@
+public class
+public static try {
+    void
+} catch (Exception e) {
+    // TODO: handle exception
+} main(String[] args) {
+    
+}
---
+++ public class Practiceifelse.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class Practiceifelse{
+
+public static void main(String arg[]) {
+scanner Hamza = new Scanner(System.in);
+
+int a= Hamza.nextInt();
+int b= Hamza.nextInt();
+
+if (a == b) {
+    System.out.println("The number is equal to b");
+}
+else {
+    if (a > b)
+    System.out.println("the number is less than b");
+    else{}
+}
+}
+}
